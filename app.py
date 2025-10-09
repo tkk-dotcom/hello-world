d300d4566a9a142d4bfe85a9534fb75725f99e46
tkk-dotcom <thottempudikrishnakumar@gmail.com>
2025-10-08 21:04:54 -0500

    Update and rename app.py to package.json

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(_name_)
-
-@app.route('/')
-def hello():
-    return "Hello World from GitHub + AKS!"
-
-if _name_ == "_main_":
-    app.run(host="0.0.0.0", port="8080")
